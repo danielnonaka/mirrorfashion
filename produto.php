f243e390428972f1352cd2d5c3e3eb5932240953
web7082 <web7082@caelumBSB01-07>
2017-05-20 11:56:51 -0300

    exercicio 8.13

--- produto.php
+++ produto.php
@@ -13,6 +13,8 @@
 
 	<body>
 		<?php include("cabecalho.php"); ?>
+	
+	<div class = "container">
 		
 		<div class="produto">
 			<h1> Fuzzy Cardigan </h1>
@@ -38,11 +40,55 @@
 					</label>
 					
 				</fieldset>
+				
+				<fieldset class="tamanhos">
+					<legend>Escolha o tamanho:</legend>
+					<input type="range" min="36" max="46" value="42" step="2" name="tamanho" id="tamanho">
+
+				</fieldset>
 
 				<input type="submit" class="comprar" value="Comprar">
 			</form>
 		</div>
 
+		<div class="detalhes">
+			<h2>Detalhes do produto</h2>
+			<p> 	Esse é o melhor casaco de Cardigã que você já viu. Excelente material italiano com estampa desenhada
+				pelos artesãos da comunidade de Krotor nas ilhas gregas. Compre já e receba hoje mesmo pela nossa
+				entrega a jato
+			</p>
+			
+			<table>
+				<thread>
+					<tr>
+						<th>Característica</th>
+						<th>Detalhe</th>
+					</tr>
+
+				</thread>
+
+				<tbody>
+					<tr>
+						<td>Modelo</td>
+						<td>Cardigã</td>
+					</tr>
+					<tr>
+						<td>Material</td>
+						<td>Algodão e poliester</td>
+					</tr>
+					<tr>
+						<td>cores</td>
+						<td>Azul, Rosa e Verde</td>
+					</tr>
+					<tr>
+						<td>Lavagem</td>
+						<td>Lavar a mão</td>
+					</tr>
+				</tbody>
+			</table>
+
+		</div>
+	</div>
 
 
 		<?php include("rodape.php"); ?>
